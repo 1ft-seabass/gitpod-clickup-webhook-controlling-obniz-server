f920e56bb49de9d7c8f8da48ff1f41207b7920a8
1ft-seabass <seigo@1ft-seabass.jp>
2021-12-23 05:01:04 +0000

    update

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const Obniz = require('obniz');
 
-const urlDiscordWebHook = '<DiscordWebHook>';
+// obniz の接続
+// <OBNIZ_ID> に、自分の使う obniz の ID を入力
+const obniz = new Obniz("<OBNIZ_ID>");
+
+// obniz 接続後にメッセージをだす
+obniz.onconnect = async function () {
+  obniz.display.clear();
+  obniz.display.print("Hello ClickUp!");
+}
+
 // public というフォルダに入れられた静的ファイルはそのまま表示
 app.use(express.static(__dirname + '/public'));
 
@@ -10,21 +20,21 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+// Clickup Webhook の受信
 app.post('/clickup/webhook', async function(req, res) {
   console.log('/clickup/webhook');
 
   console.log('タスクのタイトル');
   console.log(req.body.payload.name);
-  console.log('メッセージ内容');
+  console.log('Discord メッセージ内容');
   console.log(req.body.payload.text_content);
 
-  const payload = {
-    "content":req.body.payload.text_content
-  };
+  // ClickUp の Webhook データからタスク詳細のテキストをメッセージとして抽出
+  const messageObnizDisplay = req.body.payload.text_content;
 
-  const response = await axios.post(urlDiscordWebHook,payload);
-  console.log('response');
-  console.log(response.data);
+  // obniz のディスプレイにメッセージ反映
+  obniz.display.clear();
+  obniz.display.print(messageObnizDisplay);
 
   res.end();
 });
