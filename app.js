9ef6165932065195019e42300b3419ddf76b8787
1ft-seabass <seigo@1ft-seabass.jp>
2021-12-23 03:36:27 +0000

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // public というフォルダに入れられた静的ファイルはそのまま表示
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +9,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/clickup/webhook', function(req, res) {
-  console.log('/clickup/webhook');
-  console.log(req.body);
+  // console.log('/clickup/webhook');
+  // console.log(req.body);
+  console.log(req.body.payload.text_content);
   res.end();
 });
 app.listen(3000);
